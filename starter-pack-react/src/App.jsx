42ceb97604badc7d638f035d36359931e15aa952
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2025-02-17 22:14:08 +0600

    colors add

--- starter-pack-react/src/App.jsx
+++ starter-pack-react/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         Where can I get some?
         There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
       </h1>
-      <h2 className='title-mt font-secondary'>gggggggggggggggggggggg</h2>
+      <h2 className='title-mt font-secondary text-green-500 xl:text-primary'>gggggggggggggggggggggg</h2>
       </div>
     </div>
   )
